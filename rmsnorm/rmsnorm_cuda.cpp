cbbabc104e09d9938f17ec148ca0cf9b7045dc99
Daniel Suess <daniel@dsuess.me>
2022-01-25 10:40:27 +0000

    [WIP]

--- rmsnorm/rmsnorm_cuda.cpp
+++ rmsnorm/rmsnorm_cuda.cpp
@@ -10,19 +10,24 @@ torch::Tensor rmsnorm_cuda_forward(
 
 // C++ interface
 
+// TODO Add channels-last kernel
 #define CHECK_CUDA(x) TORCH_CHECK(x.is_cuda(), #x " must be a CUDA tensor")
-#define CHECK_CONTIGUOUS(x) TORCH_CHECK(x.is_contiguous(), #x " must be contiguous")
-#define CHECK_INPUT(x) CHECK_CUDA(x); CHECK_CONTIGUOUS(x)
+#define CHECK_CONTIGUOUS(x) TORCH_CHECK(x.is_contiguous(), #x " must be contiguous, channels-first")
+#define CHECK_INPUT(x) \
+  CHECK_CUDA(x);       \
+  CHECK_CONTIGUOUS(x)
 
 torch::Tensor rmsnorm_forward(
     torch::Tensor input,
-    torch::Tensor weights) {
+    torch::Tensor weights)
+{
   CHECK_INPUT(input);
   CHECK_INPUT(weights);
 
   return rmsnorm_cuda_forward(input, weights);
 }
 
-PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m)
+{
   m.def("forward", &rmsnorm_forward, "rmsnorm forward (CUDA)");
 }
--- rmsnorm/rmsnorm_cuda_kernel.cu
+++ rmsnorm/rmsnorm_cuda_kernel.cu
@@ -1,13 +1,57 @@
 #include <torch/extension.h>
+#include <ATen/cuda/CUDAContext.h>
 
 #include <cuda.h>
 #include <cuda_runtime.h>
 
 #include <vector>
 
+const int MAX_BLOCK_SIZE = 512;
+
+__host__ __forceinline__ int prev_power_of_two(unsigned int n)
+{
+  n |= (n >> 1);
+  n |= (n >> 2);
+  n |= (n >> 4);
+  n |= (n >> 8);
+  n |= (n >> 16);
+  return n - (n >> 1);
+}
 
 torch::Tensor rmsnorm_cuda_forward(
     torch::Tensor input,
-    torch::Tensor weights) {
-  return input;
+    torch::Tensor weights)
+{
+  const auto batch_size = input.size(0);
+  const auto seq_len = input.size(1);
+  const auto num_channels = input.size(2);
+  auto stream = at::cuda::getCurrentCUDAStream();
+
+  // Allocate batch-variance
+  float *channel_var;
+  {
+    const auto size = sizeof(float) * batch_size * seq_len;
+    cudaMallocAsync((void **)&channel_var, size, stream);
+  }
+
+  auto out = torch::empty_like(input);
+  int block_x = max(32, min(MAX_BLOCK_SIZE, prev_power_of_two(num_channels) / 4));
+  int block_y = max(1, min(MAX_BLOCK_SIZE / block_x, prev_power_of_two(batch_size) / 4));
+  const dim3 block(block_x, block_y);
+
+  switch (input.scalar_type())
+  {
+  case at::ScalarType::Float:
+    //to sth
+    break;
+  case at::ScalarType::Half:
+    //to sth
+    break;
+  default:
+    cudaFreeAsync(channel_var, stream);
+    throw std::runtime_error("Input-dtype not supported");
+  }
+
+  cudaFreeAsync(channel_var, stream);
+  return out;
 }
